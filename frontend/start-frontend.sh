91a6c9db90a51572ded9456dd289ff6580cb8e58
Traoré Daouda <traoredaouda@MacBook-Air-de-Traore.local>
2025-11-09 14:09:03 +0100

    Clean: remove unused frontend scripts

--- frontend/start-frontend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Lancement du frontend à $(date)" >> /var/log/frontend-start.log
-/usr/local/bin/serve -s build -l 3000 >> /var/log/frontend-out.log 2>> /var/log/frontend-error.log
