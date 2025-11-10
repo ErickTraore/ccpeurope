156f689da758f549e5a852b63a40fdcff2c9e58e
Traoré Daouda <traoredaouda@MacBook-Air-de-Traore.local>
2025-11-10 07:41:30 +0100

    🔧 Mise à jour  user-backend/config/config.js

--- user-backend/config/config.js
+++ user-backend/config/config.js
@@ -1,27 +1,27 @@
-// File: media-backend/config/config.js
+// File: user-backend/config/config.js
 
 require('dotenv').config();
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_DEVELOPMENT,
-    host: process.env.DB_HOST, // force localhost
-    dialect: process.env.DB_DIALECT || 'mysql'
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME_DEVELOPMENT,
+        host: process.env.DB_HOST, // force localhost
+        dialect: process.env.DB_DIALECT || 'mysql'
 
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST, // force localhost
-    dialect: process.env.DB_DIALECT || 'mysql'
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PRODUCTION,
-    host: process.env.DB_HOST, // force localhost
-    dialect: process.env.DB_DIALECT || 'mysql'
-  }
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME_TEST,
+        host: process.env.DB_HOST, // force localhost
+        dialect: process.env.DB_DIALECT || 'mysql'
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME_PRODUCTION,
+        host: process.env.DB_HOST, // force localhost
+        dialect: process.env.DB_DIALECT || 'mysql'
+    }
 };
